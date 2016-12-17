3742ab3c2b78f7679f48b93ed141941305013855
PhenomX1998 <cool.leo.aditya@gmail.com>
2016-12-17 12:37:52 +0000

    Citrus-fy
    Signed-off-by: PhenomX1998 <cool.leo.aditya@gmail.com>

--- citrus.mk
+++ citrus.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2016 Citrus-CAF Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,11 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from gemini device
 $(call inherit-product, device/xiaomi/gemini/device.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/citrus/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := cm_gemini
+PRODUCT_NAME := citrus_gemini
 PRODUCT_DEVICE := gemini
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 5
