5484dadbaca4106bef2099f7533c8edc27f01bb0
Adarsh-MR <adarshmr1998@gmail.com>
2017-01-11 20:08:07 +0000

    Permissive for now
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,7 @@ TARGET_BOOTLOADER_BOARD_NAME := msm8996
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
